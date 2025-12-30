8911ec64723ab645b8aca81490f94a186ae89f70
swarajshaw <shaw.swaraj16@gmail.com>
2025-12-30 16:15:23 +0000

    chore: remove next build artifacts

--- admin-portal/.next/dev/server/pages/devices.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/devices.js")
-R.c("server/chunks/ssr/29edc_next_5095fee1._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__fd9ecf14._.js")
-R.c("server/chunks/ssr/29edc_next_e7df69e7._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__3687d482._.js")
-R.m("[project]/admin-portal/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/admin-portal/pages/devices.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/admin-portal/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/admin-portal/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/admin-portal/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/admin-portal/pages/devices.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/admin-portal/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/admin-portal/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
