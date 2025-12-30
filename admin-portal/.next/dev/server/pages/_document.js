a658ca2f748263b1feca5d645fb874fe08c6068d
swarajshaw <shaw.swaraj16@gmail.com>
2025-12-30 16:15:23 +0000

    chore: remove next build artifacts

--- admin-portal/.next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/29edc_next_e7df69e7._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.m("[project]/admin-portal/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/admin-portal/node_modules/next/document.js [ssr] (ecmascript)").exports
